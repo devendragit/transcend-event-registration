87f7b4f3f755eb6b0dd3ac024d411ab8e13cc661
carldprojects <carld.projects@gmail.com>
2017-09-20 11:38:36 +0800

    Changes for admin panel

--- includes/ts-acf-fields.php
+++ includes/ts-acf-fields.php
@@ -538,4 +538,47 @@ if( function_exists('acf_add_local_field_group') ) {
 		'description' => '',
 	));
 
+	acf_add_local_field_group(array (
+		'key' => 'group_598d69e58299d',
+		'title' => 'Mailchimp',
+		'fields' => array (
+			array (
+				'key' => 'field_598d6a2eb84ed',
+				'label' => 'List ID',
+				'name' => 'list_id',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array (
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'ts_tour',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => 1,
+		'description' => '',
+	));
+
 }
